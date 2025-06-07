f5544d0f62e794da194df3fdedbddbb77776a4ac
sameeksha07-07-2003 <100752202+sameeksha07-07-2003@users.noreply.github.com>
2025-06-07 21:24:55 +0000

    datatypesummaryandcontainer

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,52 @@
+// Primitive Datatypes (call by value)
+//7 types : String , Number , Boolean , null , undefined, Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+//Symbol is a special and unique data type used to create unique identifiers for object properties.
+//Main use - To avoid property name conflicts in objects — even if two symbols have the same description, they are always unique.
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id) // Symbol(123)
+console.log(anotherId)//Symbol(123)
+console.log(id == anotherId) //false
+
+const bigNumber = 3384634823846237453984936532n
+console.log(typeof bigNumber)
+
+
+//Non primitive (Reference)
+
+//Array , Object , function'
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj = {
+    name: "sameeksha",
+    age: 22,
+}
+ 
+const myFunction = function(){
+    console.log("hello champ")
+}
+ 
+console.log(typeof myFunction)
+
+// Statically Typed Language:
+// A language where variable types are declared and checked at compile-time.
+// Example (Java): int age = 25; Examples: C, C++, Java, Rust, Go, Kotlin, TypeScript.
+
+// Dynamically Typed Language:
+// A language where variable types are determined at runtime without declaring them.
+// Example (Python): age = 25 Examples: Python, JavaScript, Ruby, PHP, Lua.
+
+
---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+console.log("Sameeksha");
+console.log("Yash");
+
+const a = 0;
+const b = 0;
+
+console.log(a+b);
+
+if(a+b){
+    console.log("valid");
+}else{
+    console.log("invalid");
+}
